63f6783f557d0e597f5280f66854fc8fb7c43e51
pawinee14 <127536876+pawinee14@users.noreply.github.com>
2023-05-15 23:49:49 +0700

    Add files via upload

---
+++ gg_appscript.js
@@ -0,0 +1,37 @@
+//1hguuh4Zx72XVC1Zldm_vTtcUUKUA6iBUOoGnJUWLfqDWx5WlOJHqYkrt
+
+var firebaseUrl = "XXXXXXXXX";
+
+var CHANNEL_ACCESS_TOKEN = 'XXXXXXXXXXX'; 
+
+
+
+var line_endpoint = 'https://api.line.me/v2/bot/message/reply';
+
+function doPost(e) {
+  var json = JSON.parse(e.postData.contents);  
+  var reply_token= json.events[0].replyToken;
+  if (typeof reply_token === 'undefined') {
+    return;
+  }
+ 
+  var message = json.events[0].message.text;
+  var reply_txt = message.toLowerCase().replace(" ", "");  
+
+  if(reply_txt == "location" || reply_txt == "gps"){
+    reply_txt = 1;
+    var jsonText = { "value": {"location":  reply_txt  }};
+    var base = FirebaseApp.getDatabaseByUrl(firebaseUrl);
+    base.updateData("", jsonText);
+  }else if(reply_txt == "open"){
+    reply_txt = 1;
+    var jsonText = { "value1": {"running":  reply_txt  }};
+    var base = FirebaseApp.getDatabaseByUrl(firebaseUrl);
+    base.updateData("", jsonText);
+  }else if(reply_txt == "close"){
+    reply_txt = 0;
+    var jsonText = { "value1": {"running":  reply_txt  }};
+    var base = FirebaseApp.getDatabaseByUrl(firebaseUrl);
+    base.updateData("", jsonText);
+  }
+}
